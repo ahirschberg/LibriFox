b501a887c0ebf5abeec6d1d3b1854b9d7e6e4d3f
fccardiff <fccardiff@yahoo.com>
2015-03-23 14:47:07 -0400

    added stripHTMLTags test [currently non-functional]

--- librifox/tests/stripHTMLTags.js
+++ librifox/tests/stripHTMLTags.js
@@ -1,1 +1,7 @@
 // test
+var app = requireApp("/js/app");
+describe("stripHTMLTags", function(){
+  it('should return -1 when the value is not present', function(){
+    assert.equal("Test?", "Test?");
+  });
+});
