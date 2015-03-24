907e229fe4237bdb961089ea7fcb0380792eae7f
ahirschberg <googlestinks@ymail.com>
2015-03-23 21:36:59 -0400

    Set up a karma config file, could not get tests working--I think we need requireJS and/or chai.  Pushing so that this code will be accessible on other machine

--- librifox/tests/searchHTML.js
+++
@@ -1,8 +0,0 @@
-// Tests to check exact match searches, as well as non-exact matches
-// Requires search function to return a string
-var app = requireApp("/js/app");
-describe("newSearch", function(){
-  it('should return -1 when the value is not present', function(){
-    assert.equal("Test?", "Test?");
-  });
-});
--- librifox/tests/stripHTMLTags.js
+++ librifox/tests/stripHTMLTags.js
@@ -1,7 +1,18 @@
-// First test, used to check if stripHTMLTags is working properly
-var app = requireApp("/js/app");
-describe("stripHTMLTags", function(){
-  it('should return -1 when the value is not present', function(){
-    assert.equal("Test?", "Test?");
-  });
+// currently, calling require() throws an error
+//var assert = require("assert")
+
+/*describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})*/
+
+describe('#stripHTMLTags()', function() {
+  it('removes all angle bracket pairs and enclosing strings', function() {
+    //assert.equal('no html tags.', stripHTMLTags('<html>no<em> html</em><garbage\n-tag>tags.')); 
+  })
 });
+
